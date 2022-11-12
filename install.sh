89de920a54fdfa34fbf0f6e81e5fbb76eaa9e96e
Torsten Boettjer <57480261+torstenboettjer@users.noreply.github.com>
2022-11-12 14:11:51 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -27,8 +27,8 @@ error() {
 #######################################
 install_python() {
   echo "Installing python"
-  apt -Y install build-essential libssl-dev libffi-dev
-  #apt install python3-pip
+  apt -Y install libssl-dev libffi-dev
+  apt install python3-pip
   PYTHON_VERSION=$(python3 --version)
   pip3 install --user virtualenvmkdir ~/.virtualenvs
   cd ~/.virtualenvs
