0aa4a121b08036a0bedcdffeaf53b05a16e9eea8
Torsten Boettjer <57480261+torstenboettjer@users.noreply.github.com>
2022-11-12 09:53:40 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# Constants
+readonly CMD_SHUTDOWN="shutdown -P now; init 0"
+
+# Exit on error
+set -e
+
+#######################################
+# Print usage message and exit
+#######################################
+usage() {
+  echo "\tInstalling Dev Workspace"
+  exit 1
+}
+
+#######################################
+# Print error message and exit
+#######################################
+error() {
+  echo "$(tput setaf 1)+++ : $*$(tput sgr 0)" >&2
+  exit 1
+}
+
+#######################################
+# Main
+#######################################
+main () {
+  usage
+  echo_header "All done"
+}
+
+main "$@"
